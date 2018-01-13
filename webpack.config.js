ba39df61197bb89761bfc33fd8d4e20bb367b180
_Ghostrick_ <ghostrick.kinako@gmail.com>
2018-01-13 23:33:28 +0900

    change directory structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
 const webpack = require('webpack'),
     glob = require('glob'),
     HtmlWebpackPlugin = require('html-webpack-plugin'),
+    HtmlWebpackExcludeAssetsPlugin = require('html-webpack-exclude-assets-plugin'),
     entries = {}
+
+const entryJsPath = `${__dirname}/src/js/entry`,
+    outputJsExt = 'min.js'
 
 
 // create entries object
-glob.sync(`${__dirname}/src/js/**/index.js`).some(item =>  {
-    const prefix = item.replace(`${__dirname}/src/js/`, '').split('/')[0]
-    entries[prefix] = '.' + item.replace(`${__dirname}/src`,'')
+glob.sync(`${entryJsPath}/**.js`).some(item =>  {
+    const prefix = item.replace(`${entryJsPath}/`, '').replace('.js', '')
+    entries[prefix] = `./js/entry/${prefix}.js`
 })
+
 
 // create output HTML rule
 const outputHtml = Object.keys(entries).map(key => {
+
+    const ignoreReg = new RegExp(`^(?!.*${key}.${outputJsExt}$).+$`)
+
     return new HtmlWebpackPlugin({
         filename: key === 'index' ? 'index.html' : `${key}/index.html` ,
-        template: `${key}.pug`
+        template: `${key}.pug`,
+        excludeAssets: [ ignoreReg ]
     })
 })
 
@@ -24,7 +33,7 @@ module.exports = {
     entry: entries,
     output: {
         path: `${__dirname}/dist`,
-        filename: "js/[name].min.js"
+        filename: `js/[name].${outputJsExt}`
     },
     devServer: {
         contentBase: './dist',
@@ -44,6 +53,7 @@ module.exports = {
         ]
     },
     plugins: [
-        ...outputHtml
+        ...outputHtml,
+        new HtmlWebpackExcludeAssetsPlugin()
     ]
 }
